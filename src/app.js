537f6275b6f8c78c836353a2cda654b7a55264c1
Ramiro <ramazzucco@hotmail.com>
2020-11-11 15:03:56 -0300

    comment cors on app

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const methodOverride =  require('method-override');
 // const session = require ('express-session');
 require("dotenv").config();
@@ -39,14 +39,14 @@ app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(cors())
-app.get('/api/dashboard', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+// app.use(cors())
+// app.get('/api/dashboard', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 
 // catch 404 and forward to error handler
