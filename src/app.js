2aee1edc4a54ba940a0f54ba0ca9844b77e8c0e1
Ramiro <ramazzucco@hotmail.com>
2020-12-09 17:39:40 -0300

    CORS problem4

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.get('api-futbol5.herokuapp.com/api', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
