2addce90f355f885831e427b20952cd0a48303f7
Ramiro <ramazzucco@hotmail.com>
2020-12-10 19:56:28 -0300

    variables3

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,10 +17,10 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": process.env.PROD_DB_USER,
-    "password": process.env.PROD_DB_PASS,
-    "database": process.env.PROD_DB_NAME,
-    "host": process.env.PROD_DB_HOST,
+    "username": "b608968b2f2ba2",
+    "password": "fb07e3a4",
+    "database": "heroku_c26957e453bd190",
+    "host": "us-cdbr-east-02.cleardb.com",
     "dialect": "mysql",
     "operatorsAliases": 0
   }
