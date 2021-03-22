9ca20cc1fe0111afd8b7813a33b73e602862c400
Ramiro <ramazzucco@hotmail.com>
2021-03-22 16:11:01 -0300

    asd123

--- src/routes/api/admin.js
+++ src/routes/api/admin.js
@@ -7,7 +7,7 @@ router.post("/create",validationFormUsers, controllers.create);
 
 router.post("/login", controllers.login);
 
-router.post("/logout", controllers.logout);
+// router.post("/logout", controllers.logout);
 
 router.put("/changepassword", controllers.updatePassword);
 
